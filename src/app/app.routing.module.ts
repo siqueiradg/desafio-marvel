6cb813dbc7b235da2128ea2a9576431a32994911
Douglas Siqueira <douglasodimar@yandex.com>
2017-09-02 11:02:45 -0400

    Adicionado Modulo de Rotas & Logo

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home',
+    loadChildren: 'app/home/home.module#HomeModule',
+  },
+  // { path: 'question',
+  //   loadChildren: 'app/question/question.module#QuestionModule',
+  // },
+  // { path: 'score',
+  //   loadChildren: 'app/score/score.module#ScoreModule',
+  // },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
