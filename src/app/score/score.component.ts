f2eb17bee496c17962ac53d2bf9d8e2582c67003
Douglas Siqueira <douglasodimar@yandex.com>
2017-09-03 19:14:55 -0400

    Finalizado modulo score

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -10,12 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class ScoreComponent implements OnInit {
 
   name: string;
-  score: number;
+  score: string;
+  url: string;
 
   constructor() { }
 
   ngOnInit() {
     this.name = localStorage.getItem('namePlayer');
-    this.score = 9;
+    this.score = localStorage.getItem('finalScore');
+    this.url = this.getUrl();
+  }
+
+  getUrl(): string {
+    return window.location.href.toString();
   }
 }
